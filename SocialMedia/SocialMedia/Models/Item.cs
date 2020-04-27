1b6e12040b75deeac4fd4d9198134c037f0315ae
SH SOHEL <49935124+shsohel@users.noreply.github.com>
2020-04-27 19:45:37 +0600

    Add project files.

---
+++ SocialMedia/SocialMedia/Models/Item.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SocialMedia.Models
+{
+    public class Item
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
+    }
+}
